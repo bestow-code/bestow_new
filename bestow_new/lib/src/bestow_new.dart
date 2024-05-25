b252c0e30c7c1e2c38cbb004f7a1d8156a0f9a46
ryanhanks-bestow <ryan.hanks@bestow.world>
2024-05-25 18:51:49 -0500

    wip

--- bestow_new/lib/src/bestow_new.dart
+++ bestow_new/lib/src/bestow_new.dart
@@ -1,3 +1,4 @@
+import 'package:objectbox/objectbox.dart';
 /// {@template bestow_new}
 /// A general storage solution
 /// {@endtemplate}
@@ -5,3 +6,24 @@ class BestowNew {
   /// {@macro bestow_new}
   const BestowNew();
 }
+
+///
+class Item implements Containable {}
+
+///
+
+final class Image {
+  late ToOne<Item> item;
+}
+
+///
+final class Container implements Containable {}
+
+///
+sealed class Containable {}
+
+///
+class Containment {}
+
+///
+class ContainmentStatus {}
